812b1ed78961623946290a135d56b71d107aaacf
Carlos-Blanco <carlos.blanco07@inacapmail.cl>
2025-08-18 22:07:44 -0400

    clase 18/08/2025

---
+++ repaso/tipos_datos.py
@@ -0,0 +1,61 @@
+#nombre=input("ingresa tu nombre: ")
+#edad=input("ingresa tu edad: ")
+#edadint=int(edad)
+
+#print("El nombre del usuariop es : ",nombre.upper())
+#print("La edad del usuario es: ", edad)
+
+
+# import math
+# from re import match
+
+
+# variableuno=int(input("Ingresa el primer numero: "))
+# variabledos=int(input("Ingresa el segundo numero: "))
+
+# print("La suma de los números Ingresados es: ", variableuno + variabledos)
+# print("La suma de los números Ingresados es: ", variableuno - variabledos)
+# print("La suma de los números Ingresados es: ", variableuno * variabledos)
+#print("La suma de los números Ingresados es: ", math.trunc(variableuno / variabledos,2))
+
+# alumnos=["Bastian","Ruth","Samuel","Carlos","Rolando"]
+# #print(alumnos)
+# alumnos.append("alondra")
+# #print (alumnos)
+# print (alumnos[0])
+# alumnos.append("Michael")
+# #print(alumnos)
+# alumnosEliminado=alumnos.pop(6)
+# print(alumnos)
+
+# print("El Alumnos eliminado es ",alumnosEliminado)
+
+
+# alumnos = [
+# {"nombre":"bastian","edad":18},
+# {"nombre":"michael","edad":37},
+# {"nombre":"joaquin","edad":19}
+
+# ]
+
+# alumnos.append({"nombre":"etc", "edad":33})
+# #print(alumnos)
+
+# for alumno in alumnos:
+#     if alumno["edad"]>20:
+#      print (f"El {alumno["nombre"]} tiene {alumno["edad"]} años")
+
+# contador=0
+# while contador <10:
+#     print("contador: ",contador)
+#     contador+=1
+
+
+def DametuEdad():
+    edad=input("¿Que edad tienes?")
+    return int(edad)
+
+
+
+
+
