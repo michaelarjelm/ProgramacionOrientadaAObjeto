91a6b083b7d11db55ef90a2b674245ebc3d89785
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-08-18 22:08:07 -0400

    Clase 18/08/2025

---
+++ repaso/tipos_datos.py
@@ -0,0 +1,50 @@
+# nombre = input("ungresa tu nombre")
+
+# edad=input("ingresa tu edad")
+
+# edadInt = int(edad)
+
+# print("el nombre del usuario es: ",nombre.upper())
+# print("La edad del usuario del usuario es: ",edad)
+
+
+# variableUno=int(input("ingresa el primer número: "))
+# variableDos=int(input("ingresa el segundo número: "))
+
+# print("La suma de los numeros ingreados es: ", variableUno + variableDos)
+# print("La resta de los numeros ingreados es: ", variableUno - variableDos)
+# print("La multiplicacion de los numeros ingreados es: ", variableUno * variableDos)
+# print(f"La división de los numeros ingreados es: {variableUno / variableDos:.2f}")
+
+# alumnos = ["bastian", "Ruth", "Samuel", "Carlos", "Rolando"]
+
+# print (alumnos)
+
+# alumnos.append("Alondra")
+
+# print (alumnos)
+
+# alumnoEliminado = alumnos.pop(6)
+
+# print("El alumno Eliminado es ", alumnoEliminado)
+
+# print (alumnos)
+
+# Alumnos = [
+#     {"nombre" : "bastian", "edad": 18},
+#     {"nombre" : "Michael", "edad": 37},
+#     {"nombre" : "Joaquin", "edad": 19}  
+            
+# ]
+
+# Alumnos.append({"nombre":"etc","edad" :33})
+
+# #print (alumnos)    
+
+# for alumno in Alumnos:
+#     if alumno ['edad']>20:
+#         print(f"El alumno {alumno["nombre"]} tiene {alumno['edad']} años")
+        
+        
+
+    
