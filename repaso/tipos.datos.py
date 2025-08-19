914980b53798270fb91a872f0d485c8b6514f2f5
Samuel Morales <samo.m.g.17@gmail.com>
2025-08-18 22:08:30 -0400

    clase 18/08/2025

---
+++ repaso/tipos.datos.py
@@ -0,0 +1,75 @@
+##nombre= input("ingrese tu nombre: ") 
+
+##edad = input("ingresa tu edad: ")
+
+#edadInt=int(edad)   
+##print("el nombre de el usuario es:", nombre.upper())
+##print("la edada de este usuario es:", edad)
+
+
+
+
+##variableUno = int(input("ingrese el primer numero:"))
+##variableDos = int(input("ingrese el segundo numero: "))
+
+##import math 
+##print ("la suma de los numeros es: ", math.trunc(variableUno / variableDos,2) )
+
+
+##alumnos=["bastian", "ruth", "samuel","carlos","rolando"]
+
+##print (alumnos)
+
+##alumnos.append("alondra")
+
+##print (alumnos) 
+##print(alumnos(0))
+
+##alumnos.append("michael")
+
+##print(alumnos)
+
+##alumnosEliminado= alumnos.pop(6)
+
+##print("El alumno eliminado es: ", alumnosEliminado)
+
+##print(alumnos)
+
+#alumnos = [
+   #{ "nombre": "bastian", "edad":18},
+   #{ "nombre": "michael", "edad":37},
+   #{ "nombre": "joaquin", "edad": 19}
+#]
+
+#alumnos.append({"nombre":"etc","edad":33})
+
+##for alumno in alumnos:
+   #if (alumno["edad"])<20:
+       #print(f"el {alumno["nombre"]} tiene {alumno["edad"]} años")
+
+
+##contador = 0
+##while contador<10:
+    ##print("contador: ",contador)
+    ##contador += 1
+
+
+def dameTuNombre():
+    edad=input("¿que edad tienes?")
+    return int(edad)
+
+
+
+                                          
+
+
+
+
+
+
+
+
+
+
+
+      
