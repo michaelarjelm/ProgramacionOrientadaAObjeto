9a1f3843f2e9ef761049dc6f402a4e595c86b43a
sonia vasquez <soniavasquezromero@gmail.com>
2025-08-18 22:15:38 -0400

    clase 18/08/2025

---
+++ repaso/tipo_datos.py
@@ -0,0 +1,19 @@
+# nombre=input("ingrese tu nombre")
+
+# edad=input("ingrese tu edad")
+
+# #edadint=int(edad)
+
+# print("el nombre del usuario es: ",nombre.upper())
+# print("la edad del usuario es : ",edad)
+
+
+variableuno=int(input("ingresa el primer numero"))
+variabledos=int(input("ingresa el segundo numero"))
+
+print("la suma de los dos numeros ingresados es :", variableuno+variabledos)
+
+
+
+
+
