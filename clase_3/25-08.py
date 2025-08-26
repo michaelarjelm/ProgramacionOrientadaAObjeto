6064a4923903192cba4c7250a1a8a3a74303ffe9
Alondra Gonzalez <alondra.go15@gmail.com>
2025-08-25 21:03:48 -0400

    Actualizacion

--- clase_3/25-08.py
+++ clase_3/25-08.py
@@ -2,7 +2,8 @@
 # una clase es una plantilla o modelo
 
 # ejemplo:
-class Coche:
+
+'''class Coche:
     def __init__(self, marca, modelo):
         self.marca = marca
         self.modelo = modelo
@@ -10,9 +11,54 @@ class Coche:
         print("Coche:", self.marca, self.modelo)
         
 auto1 = Coche("Toyta", "Corlla")
-auto1.mostrar_info()
+auto1.mostrar_info()'''
+
+# ------------------estructura-----------(quizas)---
+
+# class NombreClase:
+#     def __init__(self, parametro): #constructor
+#         self.parametro = parametro
+
+#     def metodo(self):
+#         print(self.parametro)
+
+# --------------------------------------------------
         
 # ejercicio:
 
+# Crea una clase llamada Circulo que tenga:
+# Un constructor que reciba el radio como parámetro.
+# Un método llamado área que calcule y devuelva el área
+# del círculo (usa la fórmula: área = π * radio^2).
 
+'''import math
+
+class Circulo:
+    def __init__(self, radio):
+        self.radio = radio
         
+    def area(self):
+        return math.pi * (math.pow(self.radio, 2))'''
+
+# Instrucciones:
+# Crea una instancia de Circulo con radio 5.
+# Llama al método área e imprime el resultado.
+
+'''circulo = Circulo(5)
+area = circulo.area()
+print("El area es:", area)'''
+
+# Ejercicio 2
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    def presentarse(self):
+        print(f"Hola, me llamo {self.nombre} y tengo {self.edad} años.")
+        
+persona1 = Persona("Rovin", 27)
+persona2 = Persona("Foster", 30)
+
+persona1.presentarse()
+persona2.presentarse()
