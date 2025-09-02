95c49fc0faa63fbb71868cd1a2c2ce2f5fb257b1
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-09-01 22:04:00 -0400

    clase 01 -09

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+
+from ProgramacionOrientadaAObjeto.repaso.persona import Persona
+
+
+persona1 = Persona()
+
+persona1.CambiarNombre
+
+print(persona1.nombreCompleto)
---
+++ repaso/cocheelectrico.py
@@ -0,0 +1,10 @@
+#Clase CocheElectrico
+class CocheElectrico:
+    def __init__(self, bateria, energia):
+        self.__bateria = bateria
+        self.__energia = energia
+    
+    def Cargadecombustible(self):
+        if __bateria in
+        
+    def Conducir(self):
